82427e4e24869229003539c5d7fe8ea0dc92f56f
Josue Baez <josuejbaeze4@gmail.com>
2024-02-19 18:04:11 -0400

    Manejo y optimización de imágenes con Next Image

--- src/components/Home/Description/Description.tsx
+++ src/components/Home/Description/Description.tsx
@@ -1,11 +1,22 @@
+import styles from './Description.module.sass'
+import Image from 'next/image'
+
 export const Descrition = () =>  {
+
     return (
-        <section>
-            <h2>Descripción</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam, quasi.
-            </p>
+        <section className={styles.Description}>
+            <Image // variante de etiqueta img en NEXT que permite aplicaciones de dif filtros y mejor optimización
+                src="/images/description.jpeg"
+                alt="products marketplace"
+                width={500}
+                height={300} 
+                priority={true} // viene por default en false para cargar de ultimo la imagen
+                quality={50} 
+            />
+            <div className={styles.Description__text}>
+                <h2>Bring the future today</h2>
+                <p>Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
+            </div>
         </section>
     )
 }
--- src/components/Shared/Header/Header.tsx
+++ src/components/Shared/Header/Header.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import styles from './Header.module.sass'
 
 export const Header = () => {
     return (<header>
             <nav>
-                <ul>
+                <ul className={styles.Header__list}>
                     {/* Elemento Link para una NAVEGACIÓN + fluida, para evitar recargar la página  */}
                     <Link href="/"> {/* utilizada para navegación interna, "a" se recomienda para externo */}
                     <li>Home</li>
