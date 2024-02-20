15d1623399a583db42eacc780684242911b11147
Josue Baez <josuejbaeze4@gmail.com>
2024-02-20 15:18:39 -0400

    Optimización de fuentes con Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import { Header } from "app/components/Shared/Header";
 import { Footer } from "app/components/Shared/Footer"; // al importarse como objeto nos ayuda con mayor info al romperse el componete
 import "./../sass/globals.sass";
-const inter = Inter({ subsets: ["latin"] });
+
+const roboto = Roboto({
+  weight: ["100", "300", "500", "700"],
+  subsets: ["latin"],
+})
 
 export default function RootLayout({
   children,
@@ -12,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <Header/>  
         {children}
         <Footer/>
