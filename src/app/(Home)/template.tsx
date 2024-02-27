cb6dc9e3a40284567bbf159414f52e73baa46b02
Josue Baez <josuejbaeze4gmail.com>
2024-02-27 18:31:06 -0400

    layout o template en Next.js

--- src/app/(Home)/template.tsx
+++ src/app/(Home)/template.tsx
@@ -1,7 +1,13 @@
+"use client"
+
 import { Descrition } from "app/components/Home/Description";
 import { Hero } from "app/components/Home/Hero";
+import { useEffect } from "react";
 
 export default function HomeLayout( {children} : {children: React.ReactNode}) {
+    useEffect(() => {
+        console.log('enviando metricas...');
+    }, [])
     return (
         <div>
             <Hero/>
