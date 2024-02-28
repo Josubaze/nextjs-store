5d0094aacdd03662ebe082d97b625634e5f2ce57
Josue Baez <josuejbaeze4gmail.com>
2024-02-28 17:40:04 -0400

    Arquitectura profesional para data fetching en un proyecto

--- src/app/(Home)/layout.tsx
+++ src/app/(Home)/layout.tsx
@@ -2,12 +2,9 @@
 
 import { Descrition } from "app/components/Home/Description";
 import { Hero } from "app/components/Home/Hero";
-import { useEffect } from "react";
+
 
 export default function HomeLayout( {children} : {children: React.ReactNode}) {
-    useEffect(() => {
-        console.log('enviando metricas...');
-    }, [])
     return (
         <div>
             <Hero/>
