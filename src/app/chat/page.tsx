613035ca3d2191f909b167ec72833ca5ac6e1823
Josue Baez <josuejbaeze4gmail.com>
2024-04-05 19:43:01 -0400

    Creando componente de chatbot de ventas con la SDK IA de Vercel

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,11 @@
+import { Chat } from "src/components/chat/chat"
+
+export default function ChatPage(){
+    return (
+        <>
+            <h1>ChatBot</h1>
+            <Chat></Chat>
+        </>
+        
+    )
+}
