d6f94e28630b07d9bdb5f332906c2aa803c902f0
Josue Baez <josuejbaeze4gmail.com>
2024-04-08 15:51:16 -0400

    Creando componente de chatbot de ventas con la SDK IA de Vercel

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,20 +1,21 @@
 'use client';
-
+ 
 import { useChat } from 'ai/react';
 import styles from "./chat.module.sass";
 
 export const Chat = () => {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+ 
   return (
-    <div>
+    <div className={styles.chat_container}>
       {messages.map(m => (
-        <div key={m.id}>
+        <div key={m.id} className={styles.chat_container__message}>
           {m.role === 'user' ? 'User: ' : 'AI: '}
           {m.content}
         </div>
       ))}
  
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className={styles.chat_container__form}>
         <input
           value={input}
           placeholder="Say something..."
