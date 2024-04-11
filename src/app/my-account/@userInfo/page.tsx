d78e9425b28c101e0eb8043036ce47c55d7fbf4e
Josue Baez <josuejbaeze4gmail.com>
2024-04-11 16:26:07 -0400

    export force-dynamic

--- src/app/my-account/@userInfo/page.tsx
+++ src/app/my-account/@userInfo/page.tsx
@@ -1,11 +1,12 @@
 import { validateAccessToken } from "src/utils/auth/validateAccessToken";
 
+export const dynamic = 'force-dynamic'
 export default async function MyAccountPage() {
   const customer = await validateAccessToken();
   return (
     <div>
       <section>
-        <h2>Your info</h2>
+        <h2>Tu Información</h2>
         <h1>Bienvenido {customer?.firstName}</h1>
         <p>email: {customer?.email}</p>
       </section>
